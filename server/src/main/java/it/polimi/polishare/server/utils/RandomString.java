298fb54ff348381d30fdf99407dfbdd4a00f39d3
Daddeee <daddemc@gmail.com>
2018-07-06 18:35:37 +0200

    RandomString unit test

--- server/src/main/java/it/polimi/polishare/server/utils/RandomString.java
+++ server/src/main/java/it/polimi/polishare/server/utils/RandomString.java
@@ -16,13 +16,13 @@ public class RandomString {
         return new String(buf);
     }
 
-    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static final String lower = upper.toLowerCase(Locale.ROOT);
+    private static final String lower = upper.toLowerCase(Locale.ROOT);
 
-    public static final String digits = "0123456789";
+    private static final String digits = "0123456789";
 
-    public static final String alphanum = upper + lower + digits;
+    private static final String alphanum = upper + lower + digits;
 
     private final Random random;
 
@@ -38,23 +38,14 @@ public class RandomString {
         this.buf = new char[length];
     }
 
-    /**
-     * Create an alphanumeric string generator.
-     */
     public RandomString(int length, Random random) {
         this(length, random, alphanum);
     }
 
-    /**
-     * Create an alphanumeric strings from a secure generator.
-     */
     public RandomString(int length) {
         this(length, new SecureRandom());
     }
 
-    /**
-     * Create session identifiers.
-     */
     public RandomString() {
         this(20);
     }
--- server/src/test/java/it/polimi/polishare/server/utils/RandomStringTest.java
+++ server/src/test/java/it/polimi/polishare/server/utils/RandomStringTest.java
@@ -1,5 +1,18 @@
+package it.polimi.polishare.server.utils;
+
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class RandomStringTest {
+    @Test
+    public void nextString() {
+        final int length = 10;
+        final RandomString randomString = new RandomString(length);
 
+        String result = randomString.nextString();
+
+        assert(result != null && result.length() == length);
+    }
 }
